e76e37b7b098e9794f1b2c369113a704ff04876e
VoidUpdate <matthew.w.shaw74@gmail.com>
2020-03-17 09:54:33 +0000

    Moved tree models into separate gameobjects, Started work on scripts (MouseSelect to send event for selected gameobject)

---
+++ Assets/Scripts/Events.cs
@@ -0,0 +1,5 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class GameObjectEvent : UnityEvent<GameObject> { } // Can pass a GameObject as an argument in the event
---
+++ Assets/Scripts/MouseSelect.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Camera))]
+public class MouseSelect : MonoBehaviour
+{
+
+    public GameObjectEvent ItemSelect;
+
+    private Ray ray;
+    private RaycastHit hit;
+    private Camera camera;
+
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ray = camera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                print("Clicked on " + hit.collider.name);
+                ItemSelect.Invoke(hit.collider.gameObject);
+            }
+        }
+    }
+}
